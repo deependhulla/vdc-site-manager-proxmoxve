542afa0c41dcb23ecf1d361cccfe65a20a573d3b
Deepen Dhulla <deepen@deependhulla.com>
2023-12-02 17:02:14 +0530

    update on Sat Dec  2 05:02:14 PM IST 2023

--- files/extra-files/vdc-git-update.sh
+++ files/extra-files/vdc-git-update.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd /opt/vdc-site-manager-proxmoxve
-/bin/cp -pRv /usr/share/webmin/vdcsitemanager  /opt/vdc-site-manager-proxmoxve/files/rootdir-webmin/usr/share/webmin/
-/bin/cp -pRv /usr/local/src/vdcsitemanager-tools  /opt/vdc-site-manager-proxmoxve/files/rootdir-webmin/usr/local/src/
+rsync -av /usr/share/webmin/vdcsitemanager  /opt/vdc-site-manager-proxmoxve/files/rootdir-webmin/usr/share/webmin/
+rsync -av /usr/local/src/vdcsitemanager-tools  /opt/vdc-site-manager-proxmoxve/files/rootdir-webmin/usr/local/src/
 
 gitdone.sh
 
